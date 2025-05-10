b14faea1497ef6df69bd0f6124139b038e3f4710
AYanovDev <alex.ianovskii@mail.ru>
2025-05-10 22:01:26 +0500

    functionality for name, email and phone inputs added

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -4,8 +4,19 @@ import "./Cv.css";
 function Cv(props) {
   return (
     <div className="cv_section">
-      <h2>This is the CV section</h2>
+      <h1>Personal Details</h1>
+      <h2>Name</h2>
       <p className="name">{props.data.name}</p>
+      <h2>E-Mail address</h2>
+      <p className="email">{props.data.email}</p>
+      <h2>Phone Number</h2>
+      <p className="phone">{props.data.phone}</p>
+      <hr />
+      <h1>Educational Experience</h1>
+      <p className="educational"></p>
+      <hr />
+      <h1>Practical Experience</h1>
+      <p className="practical"></p>
     </div>
   );
 }
--- src/Entry.jsx
+++ src/Entry.jsx
@@ -2,20 +2,69 @@ import { useState } from "react";
 import "./Entry.css";
 
 function Entry(props) {
-  const [value, setValue] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-  function submit() {
-    props.setData({ name: value });
+  function submit(e) {
+    const field = e.currentTarget.className;
+    props.setData((prevData) => {
+      if (field === "name") {
+        return { ...prevData, name };
+      } else if (field === "email") {
+        return { ...prevData, email };
+      } else if (field === "phone") {
+        return { ...prevData, phone };
+      }
+    });
   }
 
   return (
     <div className="entry_section">
+      <h1>Personal details</h1>
+      <h2>Name</h2>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button className="name" onClick={submit}>
+        Submit
+      </button>
+      <h2>E-Mail address</h2>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button className="email" onClick={submit}>
+        Submit
+      </button>
+      <h2>Phone Number</h2>
+      <input
+        type="text"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <button className="phone" onClick={submit}>
+        Submit
+      </button>
+      {/*<h1>Educational Experience</h1>
       <input
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
       <button onClick={submit}>Submit</button>
+      <button>Add more</button>
+      <h1>Practical Experience</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={submit}>Submit</button>
+      <button>Add more</button> */}
     </div>
   );
 }
